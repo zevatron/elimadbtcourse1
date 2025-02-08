3aa42f4e4411b2b845a54101a798bbc699a0a550
José Ricardo <zevatron@zevatron.com.br>
2025-02-08 14:20:44 -0300

    Inclusão, uso e conversão para o source

--- dbt_project/models/src/src_nation.sql
+++ dbt_project/models/src/src_nation.sql
@@ -3,4 +3,4 @@ select
     N_NAME,
     N_NATIONKEY,
     N_REGIONKEY
-from SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.NATION
+from {{ source('SNOWFLAKE_SOURCE', 'nation') }}
--- dbt_project/models/src/src_region.sql
+++ dbt_project/models/src/src_region.sql
@@ -1,9 +1,5 @@
-with src_region_cte as (
-    select
-        R_COMMENT,
-        R_NAME,
-        R_REGIONKEY
-    from SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.REGION
-)
-
-select * from src_region_cte
+select
+    R_COMMENT,
+    R_NAME,
+    R_REGIONKEY
+from {{ source('SNOWFLAKE_SOURCE', 'region') }}
